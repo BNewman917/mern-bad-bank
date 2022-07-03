a6a48d09798982214dbeeb0bdc060a4fc8c7aa52
BNewman917 <brentnewman917@gmail.com>
2022-07-03 16:55:38 -0500

    try again

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const colors = require("colors");
+const dotenv = require("dotenv").config();
 
 connectDB();
 

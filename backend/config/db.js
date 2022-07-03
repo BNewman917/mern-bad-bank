a9b051bebe6809cbd8cffa517937e7e3707250cb
BNewman917 <brentnewman917@gmail.com>
2022-07-03 17:58:45 -0500

    await

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     try {
         console.log(`MONGO_URI: ${process.env.MONGO_URI}`.green.underline.bold);
 
-        const conn = mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MONGO_URI: ${process.env.MONGO_URI}`.green.underline.bold);
         console.log(

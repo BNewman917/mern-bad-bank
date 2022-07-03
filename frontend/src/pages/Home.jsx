ded53c8194500ae8b8a76475601317a26d56b6e9
BNewman917 <brentnewman917@gmail.com>
2022-07-02 23:19:47 -0500

    icons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,10 +7,33 @@ export const Home = () => {
             align="center"
             txtcolor="black"
             header="A Better Bank"
-            title="Welcome to the Better Bank!"
+            title="Welcome to BetterBank!"
             text="We were bad, but we got better."
             body={
-                <img src={bank} className="img-fluid" alt="Responsive image" />
+                <>
+                    <img
+                        src="https://img.icons8.com/external-photo3ideastudio-solid-photo3ideastudio/512/000000/external-bank-public-service-photo3ideastudio-solid-photo3ideastudio.png"
+                        className="img-fluid"
+                        alt="Responsive image"
+                    />
+                    <br />
+                    <div
+                        style={{
+                            fontSize: ".6rem",
+                        }}
+                    >
+                        <a
+                            target="_blank"
+                            href="https://icons8.com/icon/tx3AdinOQ2kf/bank"
+                        >
+                            Bank
+                        </a>{" "}
+                        icon by{" "}
+                        <a target="_blank" href="https://icons8.com">
+                            Icons8
+                        </a>
+                    </div>
+                </>
             }
         />
     );

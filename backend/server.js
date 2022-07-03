2dac12edbb288cc45153840280e935fc8e29b6ef
BNewman917 <brentnewman917@gmail.com>
2022-07-03 16:09:34 -0500

    make it better

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
     app.get("*", (req, res) => {
         res.sendFile(
-            path.resolve(__dirname, "../", "frontend", "build", "index.html")
+            path.resolve(__dirname, "../", "frontend", "", "index.html")
         );
     });
 } else {

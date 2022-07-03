06b51d4b89bf5a974032f7877d9a9ef05f753c00
BNewman917 <brentnewman917@gmail.com>
2022-07-03 17:02:44 -0500

    try again

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
     app.get("*", (req, res) => {
         res.sendFile(
-            path.resolve(__dirname, "../", "frontend", "", "index.html")
+            path.resolve(__dirname, "../", "frontend", "build", "index.html")
         );
     });
-} else {
-    app.get("/", (req, res) => res.send("App not set to production"));
 }
 
 app.use(errorHandler);

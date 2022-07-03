9a173dbd578734374b8777934010d95a24cf231a
BNewman917 <brentnewman917@gmail.com>
2022-07-03 15:48:09 -0500

    make it better

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
 
 const connectDB = async () => {
     try {
+        console.log(`MONGO_URI: ${process.env.MONGO_URI}`.green.underline.bold);
+
         const conn = await mongoose.connect(process.env.MONGO_URI);
 
+        console.log(`MONGO_URI: ${process.env.MONGO_URI}`.green.underline.bold);
         console.log(
             `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
         );
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,11 @@ const path = require("path");
 const express = require("express");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
-
-const corsOptions = {
-    origin: "http://localhost:3000",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

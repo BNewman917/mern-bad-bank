6ada9a0b832940c62a1e1137663703a8077edfcc
BNewman917 <brentnewman917@gmail.com>
2022-07-04 10:54:30 -0500

    port

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,6 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(process.env.PORT, () =>
+    console.log(`Server started on port ${port}`)
+);

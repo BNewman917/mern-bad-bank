79a2750d4297ce184b85da914f2bb0776526f325
BNewman917 <brentnewman917@gmail.com>
2022-07-03 19:51:34 -0500

    try more

--- frontend/src/features/capitalize.js
+++ frontend/src/features/capitalize.js
@@ -1,7 +1,3 @@
 export const capitalize = (string) => {
-    const words = string.split(" ");
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-    }
-    return words.join(" ");
+    return string.charAt(0).toUpperCase() + string.slice(1);
 };
